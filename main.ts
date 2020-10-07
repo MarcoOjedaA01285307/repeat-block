eae3f67b04b01c9eb8bef4026782467f5dda3136
MarcoOjedaA01285307 <70342450+MarcoOjedaA01285307@users.noreply.github.com>
2020-10-07 13:55:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
+}
+basic.forever(function () {
+	
+})
